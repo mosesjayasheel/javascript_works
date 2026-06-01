5c34ff5d3a40b94d1daa00cd2d8b4120c28e63bc
mosesjayasheel <moses.jayasheel27@gmail.com>
2026-06-01 14:31:21 +0530

    Practice

---
+++ JS Intro/app.js
@@ -0,0 +1,53 @@
+// console.log("Hello World")
+
+// let name="moses"
+//     name="jayasheel"
+// console.log(name)
+// console.log(typeof name)
+
+// var rank=27
+// var rank=25
+// console.log(rank)
+// console.log(typeof rank)
+//                     // in var redclaration and reassigning is possible
+// var runs=37
+//     runs=35
+// console.log(runs)
+// console.log(typeof runs)
+
+// // const marks=57
+// // const marks=95 //redclaration is not possible
+// // console.log(marks)
+
+// // const score=75
+// //     score=67        //reassigning is also not possible
+// // console.log(score)
+
+// const marks=57
+// console.log(marks)
+
+const name="Moses Jayasheel"
+console.log(name)
+console.log(typeof name)
+
+let age=24
+console.log(age)
+console.log(typeof age)
+
+let isStudent=true
+console.log(isStudent)
+console.log(typeof isStudent)
+
+var course="Mechanical Engineering"
+console.log(course)
+console.log(typeof course)
+
+var skills="HTML, CSS, JS"
+console.log(skills)
+console.error("Error Found")
+
+let job="working"
+console.log(job)
+console.warn("Warning")
+
+console.log(typeof salary)
