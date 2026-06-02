d6443f3c4e7aa3e5999f8267819ee3a5d168e40c
mosesjayasheel <moses.jayasheel27@gmail.com>
2026-06-02 21:54:55 +0530

    Loops Practice

---
+++ Loops/app.js
@@ -0,0 +1,32 @@
+// for(i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// let i = 1;
+// while (i <=20) {
+//     console.log(i);
+//     i++;
+// }
+
+// for(let i = 1; i <= 5; i++){
+// console.log("Welcome");
+// }
+
+// for ( i = 2; i < 10; i+=2) {
+//     console.log(i);
+// }
+
+// for ( i = 1; i <= 10; i += 2) {
+//     console.log(i);
+// }
+
+// for ( i = 10; i >= 0; i-- ) {
+//     console.log(i);
+// }
+
+let i = 1;
+
+do {
+    console.log(i);
+    i++;
+} while (i <= 10);
