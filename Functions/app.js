c8b47b77f1c7218106af663ef51e5685b89ebff3
mosesjayasheel <moses.jayasheel27@gmail.com>
2026-06-04 23:50:57 +0530

    Functions Practice

---
+++ Functions/app.js
@@ -0,0 +1,33 @@
+// function name() {
+//     console.log("Moses");
+// }
+// name();
+
+// function greet(name) {
+//     console.log("welcome" + " " + name);
+// }
+// greet("Moses");
+
+// function add(a,b) {
+//     return a+b;
+// }
+// let addition = add(10,20);
+// console.log(addition);
+
+
+const add = (a, b) => {
+return a + b;
+}
+const addition = add(20, 21);
+console.log(addition);
+
+const subtract = (c,d) => {c-d}
+const subtraction = (30,10);
+console.log(subtraction);
+
+
+const bmi = (weight,height) => {
+    return weight/height**2;
+}
+let total =  bmi(95,6);
+console.log(total);
