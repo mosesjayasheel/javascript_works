848727e55b6aabdf897d633c705be52828da016b
mosesjayasheel <moses.jayasheel27@gmail.com>
2026-06-04 14:23:17 +0530

    Nested lops

--- Loops/app.js
+++ Loops/app.js
@@ -24,9 +24,41 @@
 //     console.log(i);
 // }
 
-let i = 1;
+// let i = 1;
 
-do {
-    console.log(i);
-    i++;
-} while (i <= 10);
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
+
+// for(let i=1; i<=5; i++) {
+//     for( let j=1; j<=5; j++ ) {
+//         console.log(i, j)
+//     }
+// }
+
+// let i=1;
+
+// for (let j = 1; j <= 5; j++) {
+//     if (j === 5) {
+//         break;
+//     }
+//     console.log(i, j);
+// }
+
+// let i =5;
+
+// for (i=5; i>=1; i--) {
+//     console.log('*'.repeat(i));
+// }
+
+// let number = 5;
+
+// for (i=1; i<=10; i++) {
+//     console.log(number * i)
+// }
+
+let number = 5;
+for (i=1; i<=10; i++) {
+    console.log(`${number} * ${i} = ${number * i}`);
+}
