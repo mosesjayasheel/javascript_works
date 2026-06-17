9f205f7a8b8ffca5359dae8a9292b27b7cd872c1
mosesjayasheel <moses.jayasheel27@gmail.com>
2026-06-17 14:36:49 +0530

    DOM

---
+++ DOM/app.js
@@ -0,0 +1,18 @@
+// let heading = document.getElementById("title")
+// let heading = document.querySelector(".Title")
+
+// heading.innerText = "Document Object Model"
+// heading.style.color = "red"
+// heading.style.backgroundColor = "black"
+
+
+// let para = document.querySelectorAll("p");
+
+// console.log(para[1]);
+
+let para = document.querySelector("#para");
+
+function DynamicUpdate() {
+    para.innerText = "Document Object Model"
+    para.style.color = "red"
+}
