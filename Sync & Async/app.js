e047d6c2d2d46ff5fbd534a59d321b4d834dccb6
mosesjayasheel <moses.jayasheel27@gmail.com>
2026-07-13 21:43:42 +0530

    Fetch

---
+++ Sync & Async/app.js
@@ -0,0 +1,23 @@
+// const container = document.getElementById("productbucket")
+
+// fetch("https://dummyjson.com/recipes") 
+//     .then( response => response.json() )
+//     .then(data => {
+//         console.log(data.products);
+//     })
+
+const container = document.getElementById("productbucket")
+
+fetch("https://dummyjson.com/recipes")
+    .then(response => response.json())
+    .then(data => {
+        container.innerHTML = data.recipes.map(recipes => `
+            
+    <div class = "card">
+            <h3>${recipes.name}</h3>
+            <img src = ${recipes.image}>
+            <button class = "btn"> ${recipes.rating} </button>
+        `).join("")
+    })
+
+const bg = document.getElementById(" ")
